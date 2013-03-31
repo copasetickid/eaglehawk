0ac18afb04fbe421eead135f5c8ce9afc9122e16
Rushaine McBean <codekid24@gmail.com>
2013-03-31 02:15:16 -0400

    First commit of EagleHawk

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eaglehawk::Application.config.secret_token = '519dcdd650ae179b5fdac0a0117feeb6a64965870755b45a2cc8c42f999d160b0b31c0ebb34f1edf49ce3f920f2c1baffb1e7b2aedf52f91706b8580310317f5'
